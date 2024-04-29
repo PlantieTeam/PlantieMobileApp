7b038d16c67a574f543f7eb7a6cc075e5ac1a995
ahmadQasem37 <ahmadQasem37@gmail.com>
2024-04-29 12:33:56 +0300

    Create Landing Page

---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Landing_page extends StatelessWidget {
+  const Landing_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Text('Hello World!'),
+    ));
+  }
+}
