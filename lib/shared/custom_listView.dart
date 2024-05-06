96cc9075d78e8b0856f327b0e026b3a607c5b6be
ahmadQasem37 <ahmadQasem37@gmail.com>
2024-05-06 14:02:03 +0300

    ADD Shared ListView

---
+++ lib/shared/custom_listView.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class CustomListView extends StatefulWidget {
+  const CustomListView({super.key});
+
+  @override
+  State<CustomListView> createState() => _CustomListViewState();
+}
+
+class _CustomListViewState extends State<CustomListView> {
+  List<String> iconsList = [
+    'assets/icons/apple.png',
+    'assets/icons/green-beans.png',
+    'assets/icons/corn.png',
+    'assets/icons/cucumber.png',
+    'assets/icons/grapes.png',
+    'assets/icons/pepper.png',
+    'assets/icons/potato.png',
+    'assets/icons/olives.png',
+    'assets/icons/tomato.png',
+    'assets/icons/strawberry.png',
+  ];
+  Color iconBorderButtomSide = const Color(0xff777777);
+  Color listBorderButtomSide = const Color(0xffE6E6E6);
+  int? selectedIconIndex;
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      decoration: BoxDecoration(
+          border: Border(
+              bottom: BorderSide(color: listBorderButtomSide, width: 1.1))),
+      height: (size.height) * 0.1,
+      width: size.width,
+      child: ListView.builder(
+          itemCount: iconsList.length,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return InkWell(
+              onTap: () {
+                setState(() {
+                  selectedIconIndex = index;
+                });
+              },
+              onDoubleTap: () {
+                setState(() {
+                  selectedIconIndex = null;
+                });
+              },
+              child: Container(
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(
+                              color: index == selectedIconIndex
+                                  ? iconBorderButtomSide
+                                  : listBorderButtomSide,
+                              width: 1.1))),
+                  padding:
+                      const EdgeInsetsDirectional.fromSTEB(14.0, 14.0, 14.0, 8),
+                  child: Image.asset(
+                    iconsList[index],
+                  )),
+            );
+          }),
+    );
+  }
+}
