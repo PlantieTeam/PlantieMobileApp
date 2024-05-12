44ca6944f21f283005c2b6f30e584701d6398f93
ahmadeleiwa <ahmadilawa@gmail.com>
2024-05-12 21:27:42 +0300

    Add npk prop to the Plant Model

--- lib/models/plant.dart
+++ lib/models/plant.dart
@@ -4,45 +4,65 @@ class Plant extends Equatable {
   final String name;
   final String description;
   final String image;
-
+  final List<int> npk;
   const Plant(
-      {required this.name, required this.description, required this.image});
+      {required this.name,
+      required this.description,
+      required this.image,
+      required this.npk});
 
   @override
   List<Object?> get props => [name, description, image];
 }
 
 const List<Plant> dbPlants = [
-  Plant(name: 'Apple', description: 'Fruit', image: 'assets/images/apple.png'),
+  Plant(
+      name: 'Apple',
+      description: 'Fruit',
+      image: 'assets/images/apple.png',
+      npk: [150, 75, 50]),
   Plant(
       name: 'Green Beans',
       description: 'Vegetable',
+      npk: [175, 75, 50],
       image: 'assets/images/green-beans.png'),
   Plant(
-      name: 'Corn', description: 'Vegetable', image: 'assets/images/corn.png'),
+      npk: [175, 75, 50],
+      name: 'Corn',
+      description: 'Vegetable',
+      image: 'assets/images/corn.png'),
   Plant(
+      npk: [175, 75, 50],
       name: 'Cucumber',
       description: 'Vegetable',
       image: 'assets/images/cucumber.png'),
   Plant(
-      name: 'Grapes', description: 'Fruit', image: 'assets/images/grapes.png'),
+      npk: [175, 75, 50],
+      name: 'Grapes',
+      description: 'Fruit',
+      image: 'assets/images/grapes.png'),
   Plant(
+      npk: [175, 75, 50],
       name: 'Pepper',
       description: 'Vegetable',
       image: 'assets/images/pepper.png'),
   Plant(
+      npk: [175, 75, 50],
       name: 'Potato',
       description: 'Vegetable',
       image: 'assets/images/potato.png'),
   Plant(
+      npk: [175, 75, 50],
       name: 'Olives',
       description: 'Vegetable',
       image: 'assets/images/olives.png'),
   Plant(
+      npk: [175, 75, 50],
       name: 'Tomato',
       description: 'Vegetable',
       image: 'assets/images/tomato.png'),
   Plant(
+      npk: [175, 75, 50],
       name: 'Strawberry',
       description: 'Fruit',
       image: 'assets/images/strawberry.png'),
