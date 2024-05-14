0add5d493714d02398487419c57191281082baf7
ahmadeleiwa <ahmadilawa@gmail.com>
2024-05-14 22:04:31 +0300

    Setup the firebase in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:plantie/bloc/plant_bloc.dart';
+import 'package:plantie/firebase_options.dart';
 import 'package:plantie/shared/custom_navbar_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
