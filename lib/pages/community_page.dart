c4434d15474ed6a460da58b7851ae309ac200180
ahmadeleiwa <ahmadilawa@gmail.com>
2024-06-08 02:25:10 +0300

    Update the UI

--- lib/pages/community_page.dart
+++ lib/pages/community_page.dart
@@ -1,6 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:plantie/bloc/post_bloc.dart';
+import 'package:plantie/pages/comment_page.dart';
+import 'package:plantie/shared/post_card.dart';
 
 class CommunityPage extends StatefulWidget {
   const CommunityPage({super.key});
@@ -10,15 +12,18 @@ class CommunityPage extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<CommunityPage> {
-  List<String> posts = [
-    'Post 1',
-    'Post 2',
-    'Post 3',
-  ];
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
         child: Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+      ),
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
       margin: const EdgeInsets.only(top: 5),
@@ -55,150 +60,36 @@ class _MyWidgetState extends State<CommunityPage> {
           Divider(
             color: Colors.grey.shade300,
           ),
-
-          /// posts
           Expanded(
-            child: Container(
-                width: MediaQuery.of(context).size.width * 0.98,
-                margin: const EdgeInsets.only(bottom: 50),
-                child: ListView.builder(
-                    itemCount: posts.length > 4 ? 4 : posts.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        margin: const EdgeInsets.only(top: 10),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(4),
-                          color: Colors.white,
-                        ),
-                        child: Center(
-                            child: Column(children: [
-                          Container(
-                              margin: const EdgeInsets.only(top: 10),
-                              width: MediaQuery.of(context).size.width * 0.90,
-                              child: Row(
-                                children: [
-                                  CircleAvatar(
-                                      backgroundImage: NetworkImage(FirebaseAuth
-                                          .instance.currentUser!.photoURL!)),
-                                  const SizedBox(
-                                    width: 20,
-                                  ),
-                                  Column(
-                                    children: [
-                                      Text(FirebaseAuth
-                                          .instance.currentUser!.displayName!),
-                                      const Row(
-                                        children: [
-                                          Text(
-                                            "1 d   •   ",
-                                            style: TextStyle(
-                                                color: Color(0xff777777),
-                                                fontSize: 10,
-                                                fontWeight: FontWeight.w400),
-                                          ),
-                                          Image(
-                                            image: AssetImage(
-                                                "assets/icons/tomato.png"),
-                                            width: 10,
-                                            height: 10,
-                                          ),
-                                          Text(
-                                            " Tomato",
-                                            style: TextStyle(
-                                                color: Color(0xff777777),
-                                                fontSize: 10,
-                                                fontWeight: FontWeight.w400),
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  )
-                                ],
-                              )),
-                          const SizedBox(height: 10),
-                          SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.90,
-                              child: const Text(
-                                "Hello! my corn condition is like this. What is the disease and how to deal with it? ",
-                                style: TextStyle(
-                                    fontSize: 14, fontWeight: FontWeight.bold),
-                              )),
-                          const SizedBox(height: 10),
-                          Container(
-                            width: MediaQuery.of(context).size.width,
-                            height: 200,
-                            decoration: const BoxDecoration(
-                                image: DecorationImage(
-                              image: AssetImage("assets/images/test2.png"),
-                              fit: BoxFit.cover,
-                            )),
-                          ),
-                          const SizedBox(height: 10),
-                          Container(
-                              margin: const EdgeInsets.only(bottom: 10),
-                              height: 40,
-                              width: MediaQuery.of(context).size.width * 0.90,
-                              child: Row(
-                                children: [
-                                  Container(
-                                    decoration: BoxDecoration(
-                                      color: const Color(0xffF0EFEF),
-                                      borderRadius: BorderRadius.circular(50),
-                                    ),
-                                    child: Row(
-                                      children: [
-                                        TextButton.icon(
-                                          style: TextButton.styleFrom(
-                                            foregroundColor: const Color(0xff00796A),
-
-                                              shape: const RoundedRectangleBorder(
-                                            borderRadius: BorderRadius.only(
-                                                bottomLeft:
-                                                    Radius.circular(50),
-                                                topLeft: Radius.circular(50)),
-                                          )),
-                                            onPressed: () {},
-                                            icon: const Icon(Icons.favorite),
-                                            label: const Text("5")),
-                                        Container(
-                                          // margin:
-                                          // const EdgeInsets.only(left: 10),
-                                          width: 1,
-                                          // height: double.infinity,
-                                          padding: const EdgeInsets.all(10),
-                                          color: const Color(0xffDADADA),
-                                        ),
-                                        TextButton.icon(
-                                          style: TextButton.styleFrom(
-                                            foregroundColor: const Color(0xff00796A),
-                                              shape: const RoundedRectangleBorder(
-                                            borderRadius: BorderRadius.only(
-                                                bottomRight:
-                                                    Radius.circular(50),
-                                                topRight: Radius.circular(50)),
-                                          )),
-                                          onPressed: () {},
-                                          label: const Icon(Icons.heart_broken),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-
-                                  const Spacer(),
-                                  TextButton.icon(
-                                    style: TextButton.styleFrom(
-                                      foregroundColor: const Color(0xff00796A),
-                                    ),
-                                    onPressed: () {},
-                                    icon: const Icon(Icons.message),
-                                    label: const Text("5"),
-                                  )
-                                ],
-                              ))
-                        ])),
-                      );
-                    })),
-          )
+              child: Container(
+            width: MediaQuery.of(context).size.width * 0.98,
+            margin: const EdgeInsets.only(bottom: 50),
+            child: BlocBuilder<PostBloc, PostState>(
+                bloc: BlocProvider.of<PostBloc>(context),
+                builder: (context, state) {
+                  if (state is PostLoaded) {
+                    return ListView.builder(
+                        itemCount:
+                            state.posts.length > 4 ? 4 : state.posts.length,
+                        itemBuilder: (context, index) {
+                          return PostCard(
+                              post: state.posts[index],
+                              onCommentPressed: () {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => CommentPage(
+                                          post: state.posts[index],
+                                        )));
+                              },
+                              onLikePressed: () {
+                                context
+                                    .read<PostBloc>()
+                                    .add(LikePost(id: state.posts[index].id));
+                              });
+                        });
+                  }
+                  return const Center(child: CircularProgressIndicator());
+                }),
+          ))
         ],
       ),
     ));
